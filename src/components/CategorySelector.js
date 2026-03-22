2ac09fc26f70b06af6638b0b9bdf7c531f70c332
unknown <rpfmail0@gmail.com>
2026-03-22 15:29:01 +0100

    Fix category pictogram IDs

--- src/components/CategorySelector.js
+++ src/components/CategorySelector.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const CATEGORIES = [
-  { id: 'yo', text: 'Yo', imageUrl: 'https://api.arasaac.org/api/pictograms/7339' },
-  { id: 'tu', text: 'Tú', imageUrl: 'https://api.arasaac.org/api/pictograms/2633' },
-  { id: 'quiero', text: 'Quiero', imageUrl: 'https://api.arasaac.org/api/pictograms/2816' },
-  { id: 'comer', text: 'Comer', imageUrl: 'https://api.arasaac.org/api/pictograms/2271' },
-  { id: 'beber', text: 'Beber', imageUrl: 'https://api.arasaac.org/api/pictograms/2281' },
-  { id: 'jugar', text: 'Jugar', imageUrl: 'https://api.arasaac.org/api/pictograms/3017' },
-  { id: 'ir', text: 'Ir', imageUrl: 'https://api.arasaac.org/api/pictograms/2630' },
-  { id: 'parar', text: 'Parar', imageUrl: 'https://api.arasaac.org/api/pictograms/2280' },
-  { id: 'si', text: 'Sí', imageUrl: 'https://api.arasaac.org/api/pictograms/2832' },
-  { id: 'no', text: 'No', imageUrl: 'https://api.arasaac.org/api/pictograms/2833' },
-  { id: 'feliz', text: 'Feliz', imageUrl: 'https://api.arasaac.org/api/pictograms/2573' },
-  { id: 'duele', text: 'Duele', imageUrl: 'https://api.arasaac.org/api/pictograms/2965' }
+  { id: 'yo', text: 'Yo', imageUrl: 'https://api.arasaac.org/api/pictograms/6632' },
+  { id: 'tu', text: 'Tú', imageUrl: 'https://api.arasaac.org/api/pictograms/12281' },
+  { id: 'quiero', text: 'Quiero', imageUrl: 'https://api.arasaac.org/api/pictograms/5441' },
+  { id: 'comer', text: 'Comer', imageUrl: 'https://api.arasaac.org/api/pictograms/6456' },
+  { id: 'beber', text: 'Beber', imageUrl: 'https://api.arasaac.org/api/pictograms/6061' },
+  { id: 'jugar', text: 'Jugar', imageUrl: 'https://api.arasaac.org/api/pictograms/23392' },
+  { id: 'ir', text: 'Ir', imageUrl: 'https://api.arasaac.org/api/pictograms/8142' },
+  { id: 'parar', text: 'Parar', imageUrl: 'https://api.arasaac.org/api/pictograms/7196' },
+  { id: 'si', text: 'Sí', imageUrl: 'https://api.arasaac.org/api/pictograms/5584' },
+  { id: 'no', text: 'No', imageUrl: 'https://api.arasaac.org/api/pictograms/5526' },
+  { id: 'feliz', text: 'Feliz', imageUrl: 'https://api.arasaac.org/api/pictograms/9907' },
+  { id: 'duele', text: 'Duele', imageUrl: 'https://api.arasaac.org/api/pictograms/2367' }
 ];
 
 const CategorySelector = ({ onSelectCategory }) => {
